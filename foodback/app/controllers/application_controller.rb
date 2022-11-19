6d613c7948d9c07315ce9ecf9a8ff4a99eb95c5b
Victor <vmanzanilla7@gmail.com>
2022-11-18 20:20:45 -0600

    test

--- foodback/app/controllers/application_controller.rb
+++ foodback/app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::API
-# skip_before_action :verify_authenticity_token
+ 
 include ActionController::Helpers
-helper_method :logged_in, :current_user
+helper_method :logged_in?, :current_user
 
 
-   def logged_in
+   def logged_in?
     !!current_user
    end
 
